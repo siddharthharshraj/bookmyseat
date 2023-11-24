81ca12a522fd0918ac2f87f85629ec134112a3c4
siddharthharshraj <shr6219@gmail.com>
2023-11-24 08:29:41 +0530

    Your commit message here

---
+++ src/providers/LayoutProvider.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import React, { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+function LayoutProvider({ children }: { children: React.ReactNode }) {
+  const [isAdmin, setIsAdmin] = React.useState(false);
+  const menusForAdmin = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Events",
+      path: "/admin/events",
+    },
+    {
+      title: "Bookings",
+      path: "/admin/bookings",
+    },
+    // {
+    //   title: "Users",
+    //   path: "/admin/users",
+    // },
+    {
+      title: "Reports",
+      path: "/admin/reports",
+    },
+  ];
+
+  const menusForUser = [
+    {
+      title: "Home",
+      path: "/",
+    },
+
+    {
+      title: "Bookings",
+      path: "/bookings",
+    },
+  ];
+
+  const pathname = usePathname();
+  // console.log("pathname", pathname);
+  const router = useRouter();
+  const [menusToShow, setMenusToShow] = React.useState<any[]>([]);
+  const isPrivateRoute = !["/sign-in", "/sign-up"].includes(pathname);
+  const getUserData = async () => {
+    try {
+      const response = await axios.get("/api/current-user");
+      if (response.data.user.isAdmin) {
+        setMenusToShow(menusForAdmin);
+        setIsAdmin(true);
+      } else {
+        setMenusToShow(menusForUser);
+      }
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (isPrivateRoute) {
+      getUserData();
+    }
+  }, []);
+
+  return (
+    <div className="bg-gray-200  lg:px-20 px-5">
+      {isPrivateRoute && (
+        <div className="bg-white flex justify-between items-center shadow px-3 py-5">
+          <h1
+            className=" font-semibold text-2xl cursor-pointer text-blue-900"
+            onClick={() => router.push("/")}
+          >
+            Book My Seat
+          </h1>
+
+          <div className="flex gap-5 items-center">
+            <Dropdown size="sm">
+              <DropdownTrigger>
+                <Button variant="flat" color="primary" size="sm">
+                  Profile
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                {menusToShow.map((menu) => (
+                  <DropdownItem
+                    key={menu.title}
+                    onClick={() => {
+                      router.push(menu.path);
+                    }}
+                  >
+                    {menu.title}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+      )}
+      <div className="py-5">
+        {!isAdmin && pathname.includes("admin")
+          ? "You are not authorized to view this page"
+          : children}
+      </div>
+    </div>
+  );
+}
+
+export default LayoutProvider;
